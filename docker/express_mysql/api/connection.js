603d42620e91c9b0232272624287bb3c8b5bd459
Ludwing Cano <ludwing238@gmail.com>
2024-02-29 12:13:28 -0600

    update docker connection

--- docker/express_mysql/api/connection.js
+++ docker/express_mysql/api/connection.js
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
     // host: 'localhost',
     host:'mysql',
+    port: 3306,
     user: 'chat_user',
     database: 'chat_db',
     password: 'chat_password',
