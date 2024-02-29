57dbb62b4f1be19bd6114424a15c0c71bf2cb98d
Ludwing Cano <ludwing238@gmail.com>
2024-02-29 12:35:01 -0600

    Restructurando folders

---
+++ JS/express_api_example/connection.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    // host:'mysql',
+    port: 3306,
+    user: 'chat_user',
+    database: 'chat_db',
+    password: 'chat_password',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+export default pool
---
+++ JS/express_api_example/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { getAllMessages, createMessage, createDatabase } from './db.js'
+
+const app = express()
+const port = 3000
+// This line is necessary to parse the request body
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://127.0.0.1:${port}`)
+})
+
+app.post('/messages', async (req, res) => {
+  const [username, message] = [req.body.username, req.body.message]
+  console.log(username, message);
+  const messages = await createMessage(username, message)
+  res.json(messages)
+})
+
+app.get('/messages', async (req, res) => {
+  const messages = await getAllMessages()
+  res.json(messages)
+})
+
+app.post('/start', async (req, res) => {
+  const messages = await createDatabase()
+  res.json(messages)
+})
