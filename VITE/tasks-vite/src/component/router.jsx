ec583b85bc1568f2b5e2c8f3fb36e31441aae486
Ludwing Cano <ludwing238@gmail.com>
2024-04-16 20:29:05 -0600

    adding local storage example

--- VITE/tasks-vite/src/component/router.jsx
+++ VITE/tasks-vite/src/component/router.jsx
@@ -2,7 +2,7 @@
 import TaskComponent from '../view/taskComponent'
 import PropTypes from 'prop-types';
 import AddTaskForm from '../view/addTaskForm';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Router({ ruta, setRoute }) {
 
@@ -13,6 +13,27 @@ function Router({ ruta, setRoute }) {
     const navigate = (newRoute) => {
         setRoute(newRoute);
     };
+
+    useEffect(() => {
+        // Acceder a las propiedades del sessison y del local storage
+        console.log("Local Storage: ", localStorage)
+
+
+        // Almacenar un dato en nuestro storage por medio de setItem (propiedad, valor)
+        localStorage.setItem("nombre", "Ludwing");
+        localStorage.setItem("apellido", "Cano");
+
+        // Obtener un dato de nuestro storage por medio de getItem
+       console.log(localStorage.getItem("nombre")); 
+
+        // Eliminar un dato de nuestro storage por medio de removeItem
+        localStorage.removeItem("nombre");
+        
+        // limpiar todos los datos 
+        localStorage.clear();
+    }, [])
+
+
 
     switch (ruta) {
         case "/list":
